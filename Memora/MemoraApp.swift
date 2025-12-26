98029db793cb853880a553c5651863c7bee96777
dukeyeah <dukeyeah@163.com>
2025-12-27 01:38:35 +0800

    Initial Commit

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by huayang.sun on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
